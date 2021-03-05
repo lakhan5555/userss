07fcc1288cd20742b1f964be48cece357f7c851d
Ram Lakhan Singh <62960598+lakhan5555@users.noreply.github.com>
2021-03-06 00:14:07 +0530

    Update settings.py

--- userss/settings.py
+++ userss/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '%(qhcy%lic33cexvj8ns@qm39r-7a(zy71zaikk)#!^xg_^%ps'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
